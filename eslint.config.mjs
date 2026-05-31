e8fd685aadcb5e5f270e9e45f1b54148ce63e131
Mythreya Kuricheti <git@mythreya.dev>
2026-05-31 02:06:30 -0700

    misc tooling update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,32 @@
-import typescriptEslint from "typescript-eslint";
+import tseslint from "typescript-eslint";
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint.plugin,
+export default tseslint.config(
+    {
+        ignores: ["dist/", "out/", "*.mjs", "*.js", "**/*.d.ts"],
     },
-
-    languageOptions: {
-        parser: typescriptEslint.parser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+    tseslint.configs.strictTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
     },
+    {
+        rules: {
+            "@typescript-eslint/naming-convention": ["warn", {
+                selector: "import",
+                format: ["camelCase", "PascalCase"],
+            }],
 
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
+            curly: "warn",
+            eqeqeq: "warn",
+            semi: "warn",
+            "no-throw-literal": "warn",
+            "no-eval": "error",
+            "no-caller": "error",
+            "no-duplicate-imports": "warn",
+            "no-new-wrappers": "warn",
+        },
     },
-}];
+);
