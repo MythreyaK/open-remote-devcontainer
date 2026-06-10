b02793c1f9d72efb23484985b32299b940e952af
Mythreya Kuricheti <git@mythreya.dev>
2026-06-09 23:15:14 -0700

    add settings

--- src/extension/commands.ts
+++ src/extension/commands.ts
@@ -1,20 +1,12 @@
 import * as vscode from 'vscode';
 import * as cmd from '../common/cmd';
+import { getContainerEngine } from './settings';
 
 export function showHello() {
-    vscode.window.showInformationMessage('Hello World from Open Remote - Devcontainer!');
+    vscode.window.showInformationMessage('Hello World from Open Remote - Devcontainer!' + " " + vscode.env.appRoot);
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
-export function getConfig<T>(key: string): T | undefined {
-    return vscode.workspace.getConfiguration("remote.devcontainer").get<T>(key);
-}
-
-export function getContainerEngine(): string {
-    return getConfig<string>("engine") || "docker";
-}
-
-export async function getContainerEngineVersion(log: vscode.LogOutputChannel) {
-    const { stdout } = await cmd.runCmd(getContainerEngine(), ["--version"], {}, log);
+export async function getContainerEngineVersion() {
+    const { stdout } = await cmd.runCmd(getContainerEngine(), ["--version"], {});
     vscode.window.showInformationMessage(`${getContainerEngine()} version: ${stdout}`);
 }
---
+++ src/extension/settings.ts
@@ -0,0 +1,26 @@
+import * as vscode from 'vscode';
+
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
+export function getConfig<T>(key: string): T | undefined {
+    return vscode.workspace.getConfiguration("remote.devcontainer").get<T>(key);
+}
+
+export function getContainerEngine(): string {
+    return getConfig<string>("engine") || "docker";
+}
+
+/**
+ * returns `[engine, ...engine_args]` in `engine <engine_args...>
+ * command <command args...>`
+ *
+ * e.g., `[ "podman", "--root", "<root dir>"]` for
+ * `podman --root <root dir> command <command args>`
+*/
+export function getEngineCmd(): string[] {
+    return [ getContainerEngine(), ...getExtraArgs() ];
+}
+
+
+function getExtraArgs(): string[] {
+    return (getConfig<string[]>("extraArgs") ??  []);
+}
