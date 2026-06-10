b3deb413de022619a3351de3dd47a1a27abc8b99
Mythreya Kuricheti <git@mythreya.dev>
2026-06-09 22:00:49 -0700

    add server install extension code and tests

---
+++ src/remote/installServer.ts
@@ -0,0 +1,82 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import * as vscode from 'vscode';
+
+import { runCmd } from '../common/cmd';
+
+export interface ServerInfo {
+    version: string,
+    serverUrlTemplate: string,
+};
+
+export async function getProductJson() {
+    interface ProductJson {
+        commit: string,
+        version: string,
+        serverDownloadUrlTemplate: string,
+    }
+
+    const jsonPath = path.join(vscode.env.appRoot, "product.json");
+    const jsonData = JSON.parse(await fs.readFile(jsonPath, { encoding: "utf-8", "flag": "r" })) as ProductJson;
+
+    return {
+        version: jsonData.version,
+        commit: jsonData.commit,
+        serverUrlTemplate: jsonData.serverDownloadUrlTemplate,
+    } as ServerInfo;
+}
+
+export interface ScriptInstallInfo {
+    port: number,
+    extensions: string[],
+    remoteEnvs: Record<string, string>,
+    connectionToken: string,
+    downloadTemplteUrl: string,
+    codiumVersion: string,
+    forceReinstall: boolean,
+}
+
+export function updateScript(script: string, info: ScriptInstallInfo, debug: boolean = false): string {
+    let scriptCopy = script;
+
+    if (debug) {
+        scriptCopy = script.replace("#_CODIUM_INJECT_DEBUG_SET_X", "set -x");
+    }
+
+    const extensArgs = info.extensions
+        .map(e => `--install-extension ${e}`)
+        .join(" ");
+
+    const remoteEnvs = Object.entries(info.remoteEnvs)
+        .flatMap(([k,v]) => `export ${k}=${v}`)
+        .join(";\n")
+        .concat(";\n");
+
+    scriptCopy = scriptCopy
+        .replace("CODIUM_INJECT_INSTALL_EXTENSIONS", extensArgs)
+        .replace("CODIUM_INJECT_SERVER_LISTEN_PORT", "65432")
+        .replace("CODIUM_INJECT_DOWNLOAD_URL", info.downloadTemplteUrl)
+        .replace("CODIUM_INJECT_CODIUM_INSTALL_VERSION", info.codiumVersion)
+        .replace("CODIUM_INJECT_TOKEN_VALUE", info.connectionToken)
+        .replace("CODIUM_INJECT_FORCE_REINSTALL_SERVER", info.forceReinstall ? "true" : "false")
+        .replace("#_CODIUM_INJECT_SERVER_LAUNCH_ENVS", remoteEnvs);
+
+    return scriptCopy;
+}
+
+export const INSTALL_SCRIPT_LOCATION: string = path.join(__dirname, "installServer.sh");
+
+async function generateInstallScript(info: ScriptInstallInfo, debug: boolean = false) {
+    const script = await fs.readFile(INSTALL_SCRIPT_LOCATION, { encoding: 'utf-8' });
+
+    const prodJson = await (async () => {
+        const pj = await getProductJson();
+        pj.serverUrlTemplate = pj.serverUrlTemplate
+            .replace("os", "CODIUM_OS_PLATFORM")
+            .replace("arch", "CODIUM_ARCH");
+        return pj;
+    })();
+
+    return updateScript(script, info, debug);
+}
+
---
+++ src/remote/tests/remote.test.ts
@@ -0,0 +1,64 @@
+import { describe, expect, test } from 'vitest';
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import * as assert from 'assert';
+
+import * as install from '../installServer';
+
+function countSubstring(from: string, item: string) {
+    return from.split(item).length - 1;
+}
+
+describe("Install script", () => {
+
+    test("string replacement tests / no env + no ext", async () => {
+        const info: install.ScriptInstallInfo = {
+            port: 6543,
+            extensions: [],
+            remoteEnvs: {},
+            connectionToken: "0xf00ba4",
+            downloadTemplteUrl: "https://localhost/${CODIUM_OS_PLATFORM}-${CODIUM_ARCH}.tar.gz",
+            codiumVersion: "1.2.345",
+            forceReinstall: false,
+        };
+
+        const script = await fs.readFile(install.INSTALL_SCRIPT_LOCATION, { encoding: 'utf-8' });
+        const updatedScript = install.updateScript(script, info, true);
+
+        expect(countSubstring(updatedScript, "export")).eq(0);
+        expect(updatedScript.includes("0xf00ba4")).toBe(true);
+        expect(updatedScript.includes("--install-extension")).toBe(false);
+        expect(updatedScript.includes('CODIUM_FORCE_REINSTALL_SERVER="false"')).toBe(true);
+        expect(updatedScript.includes('CODIUM_NEW_INSTALL_VERSION="1.2.345"')).toBe(true);
+        expect(updatedScript.includes('CODIUM_SERVER_LISTEN_PORT="65432"')).toBe(true);
+        expect(updatedScript.includes('localhost/${CODIUM_OS_PLATFORM}-${CODIUM_ARCH}')).toBe(true);
+
+        await fs.writeFile("out.sh", updatedScript, { encoding: 'utf-8', mode: 0o700 });
+    });
+
+    test("string replacement tests / env + ext", async () => {
+        const info: install.ScriptInstallInfo = {
+            port: 6543,
+            extensions: ["pub1.ext1", "pub2.ext1"],
+            remoteEnvs: { "ENV1": "VAL1", "ENV2": "VAL2" },
+            connectionToken: "0xf00ba4",
+            downloadTemplteUrl: "https://localhost/${CODIUM_OS_PLATFORM}-${CODIUM_ARCH}.tar.gz",
+            codiumVersion: "1.2.345",
+            forceReinstall: false,
+        };
+
+        const script = await fs.readFile(install.INSTALL_SCRIPT_LOCATION, { encoding: 'utf-8' });
+        const updatedScript = install.updateScript(script, info, true);
+
+        expect(countSubstring(updatedScript, "export")).eq(2);
+        expect(updatedScript.includes("0xf00ba4")).toBe(true);
+        expect(updatedScript.includes("--install-extension pub1.ext1 --install-extension pub2.ext1")).toBe(true);
+        expect(updatedScript.includes('CODIUM_FORCE_REINSTALL_SERVER="false"')).toBe(true);
+        expect(updatedScript.includes('CODIUM_NEW_INSTALL_VERSION="1.2.345"')).toBe(true);
+        expect(updatedScript.includes('CODIUM_SERVER_LISTEN_PORT="65432"')).toBe(true);
+        expect(updatedScript.includes('localhost/${CODIUM_OS_PLATFORM}-${CODIUM_ARCH}')).toBe(true);
+
+        await fs.writeFile("out.sh", updatedScript, { encoding: 'utf-8', mode: 0o700 });
+    });
+
+});
